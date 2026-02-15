f5ee43fd5c59f2dc0ca3fb558fb0f73df93afc3f
Serigne Ndogal <ndogal73@gmail.com>
2026-02-15 10:30:44 +0000

    Initial commit

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+host = "127.0.0.1"
+port = 5000
+
+s = socket.socket()
+s.connect((host, port))
+
+message = s.recv(1024).decode()
+print("Message du serveur :", message)
+
+s.close()
+
+print("Test Git commit")
+print("Dimanchqqe client")
+print("COMBAT FRANC")
---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+host = "127.0.0.1"
+port = 5000
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(1)
+
+print("Serveur en attente...")
+conn, addr = s.accept()
+print("Client connecté :", addr)
+
+message = "Bienvenue sur le serveur"
+conn.send(message.encode())
+
+conn.close()
+
+print ("Dimanchee  serveur")
